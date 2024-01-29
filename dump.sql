f21ad83bb596019d32938fd39426a9dbecedab42
João Vitor Sousa Ribeiro <103488539+jvribeiro98@users.noreply.github.com>
2024-01-29 15:42:17 +0000

    fist commit

---
+++ dump.sql
@@ -0,0 +1,17 @@
+const config = {
+    webhook: "WEBHOOK",
+    time: 1, 
+
+    connection: {
+        host: "127.0.0.1",
+        user: "",
+        password: "",
+        database: "",
+    },
+
+    embed: {
+        color: "BLACK"
+    }
+}
+
+module.exports = config
